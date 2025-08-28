a057f49fb48b332fc5be39524fa937959242b9c1
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2025-08-28 21:14:12 +0530

    Update script.js

--- build/script.js
+++ build/script.js
@@ -134,7 +134,7 @@ document.addEventListener("DOMContentLoaded", () => {
   `;
 
   /* ---------- BUCKET / FAV / FUTURE ---------- */
-  const bucket = ["Ice cream date 🍦","Movie marathon 🎬","Trip together ✈️","Cook together (burn the house) 🍳", "Throw you into backetball hook ⛹️‍♂️", "Etc... (no spoiler) 💌"];
+  const bucket = ["Ice cream date 🍦","Movie marathon 🎬","Trip together ✈️","Cook together (burn the house) 🍳", "Throw you into backetball hook ⛹️‍♂️", "Give you shit load of hickeys 💋", "Etc... (no spoiler) 💌"];
   document.getElementById("bucketList").innerHTML = bucket.map(it => `<li class="bucket-item">${it}</li>`).join("");
 
   document.getElementById("favGrid").innerHTML =
